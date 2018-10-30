a405280cfa421fe881b123ba6c4af6e24ce3df60
infytrainees <43363124+infytrainees@users.noreply.github.com>
2018-10-30 15:52:25 +0530

    DBMS MOCK

--- DBMS MOCK/C.sql
+++ DBMS MOCK/C.sql
@@ -2,5 +2,3 @@ select gfo.giftid,
 NVL(to_char(gfo1.orderid), 'NA') AS orders
 from gift gfo LEFT join giftorder gfo1
 on gfo.giftid = gfo1.giftid  AND gfo1.shippingcity = 'Mysore'
-
-======================Check again==============
